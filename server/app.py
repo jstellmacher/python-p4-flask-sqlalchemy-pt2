6725acf7052f9698d0b6ce8fb848b52b0d659203
Jai <jaijai.stell@gmail.com>
2023-05-27 20:59:13 -0700

    completes

--- server/app.py
+++ server/app.py
@@ -1,17 +1,77 @@
 #!/usr/bin/env python3
+# server/app.py
 
-from flask import Flask
+#!/usr/bin/env python3
+
+from flask import Flask, make_response
 from flask_migrate import Migrate
 
-from models import db
+from models import db, Pet
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Our app.config is set up to point to our existing database, and 'SQLALCHEMY_TRACK_MODIFICATIONS' is set to False to avoid building up too much unhelpful data in memory when our application is running.
 
 migrate = Migrate(app, db)
+ # migrate instance configures the application and models for Flask-Migrate.
 
 db.init_app(app)
+# db.init_app connects our database to our application before it runs.
+
+
+@app.route('/')
+def index():
+    response = make_response(
+        '<h1>Welcome to the pet/owner directory!</h1>',
+        200
+    )
+    return response
+# @app.route determines which resources are available at which URLs and saves them to the application's URL map. Responses are what we return to the client after a request. The included response has a status code of 200, which means that the resource exists and is accessible at the provided URL.
+
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+
+    if not pet:
+        response_body = '<h1>404 pet not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+
+    response_body = f'''
+        <h1>Information for {pet.name}</h1>
+        <h2>Pet Species is {pet.species}</h2>
+        <h2>Pet Owner is {pet.owner.name}</h2>
+    '''
+
+    response = make_response(response_body, 200)
+
+    return response
+
+@app.route('/owner/<int:id>')
+def owner_by_id(id):
+    owner = Owner.query.filter(Owner.id == id).first()
+
+    if not owner:
+        response_body = '<h1>404 owner not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+
+    response_body = f'<h1>Information for {owner.name}</h1>'
+
+    pets = [pet for pet in owner.pets]
+
+    if not pets:
+        response_body += f'<h2>Has no pets at this time.</h2>'
+
+    else:
+        for pet in pets:
+            response_body += f'<h2>Has pet {pet.species} named {pet.name}.</h2>'
+
+    response = make_response(response_body, 200)
+
+    return response
+#  Because we generated data with Faker, you will probably not see the same names, but your format should match below:
 
 
 if __name__ == '__main__':
--- server/seed.py
+++ server/seed.py
@@ -6,3 +6,53 @@ from faker import Faker
 
 from app import app
 from models import db, Owner, Pet
+
+fake = Faker()
+
+with app.app_context():
+
+    Pet.query.delete()
+    Owner.query.delete()
+
+    owners = []
+    for n in range(50):
+        owner = Owner(name=fake.name())
+        owners.append(owner)
+
+    db.session.add_all(owners)
+
+    pets = []
+    species = ['Dog', 'Cat', 'Chicken', 'Hamster', 'Turtle']
+    for n in range(100):
+        pet = Pet(name=fake.first_name(), species=rc(species), owner=rc(owners))
+        pets.append(pet)
+
+    db.session.add_all(pets)
+    db.session.commit()
+
+
+# ! You'll notice that this looks virtually identical to our seed scripts from Phase 3: we delete all records from the database, make lists of owner and pet instances, generate attributes with random and faker, then commit the transaction.
+
+#! Let's go over the differences:
+
+#! 1 - We need to create an application context with app.app_context() before we begin. This will not necessarily be used, but it ensures that applications fail quickly if they are not configured with this important context.
+#! 2 - Deletion of all records is handled through models with Model.query.delete() rather than session.query(Model).delete(). This syntax is carried through other SQLAlchemy statements and queries as well. Because the session is managed through the Flask application, there is no need to call it explicitly when we run these statements and queries.
+# https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/#
+
+# from models import db, Pet, Owner
+# >> from app import app
+# >> with app.app_context():
+# ...     Pet.query.all()
+# ...
+#  => [<Pet Richard, Turtle>, <Pet Jonathan, Cat>, <Pet Victoria, Chicken>, ...]
+# We can also narrow our search using filters or chains of filters:
+
+# // imports
+# >> with app.app_context():
+# = 'Ben').all()
+#  => [<Pet Owner Brenda Hernandez>, <Pet Owner Brian Stone>, ...]
+
+# >> with app.app_context():
+# ...     Owner.query.filter(Owner.name <= 'Ben').limit(2).all()
+# ...
+#  => [<Pet Owner Alan Bryant>, <Pet Owner Allison Phillips DDS>]
